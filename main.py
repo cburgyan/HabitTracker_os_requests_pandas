e89866c9a23cf982a3ea29c8c454df05389a9963
cburgyan <cburgyan@gmail.com>
2022-05-23 17:11:01 -0500

    update pixel with put

--- main.py
+++ main.py
@@ -41,15 +41,31 @@ pixela_headers = {
 
 today = datetime.datetime.now()
 print(type(today))
-previous_day = int(today.strftime("%Y%m%d")) - 1
-
+previous_day = int(today.strftime("%Y%m%d")) - 2
+previous_day2 = datetime.datetime(year=2022, month=5, day=20)
+prev_day2 = previous_day2.strftime("%Y%m%d")
+print(prev_day2)
 post_parameters = {
-    "date": str(previous_day),
-    "quantity": "361",
+    "date": str(prev_day2),
+    "quantity": "310",
 }
 
-# Create Pixel
-create_pixel_response = requests.post(url=POST_PIXEL_PIXELA_ENDPOINT,
-                                      headers=pixela_headers,
-                                      json=post_parameters)
-print(create_pixel_response.text)
+# # Create Pixel
+# create_pixel_response = requests.post(url=POST_PIXEL_PIXELA_ENDPOINT,
+#                                       headers=pixela_headers,
+#                                       json=post_parameters)
+# print(create_pixel_response.text)
+
+update_pixel_parameters = {
+    "quantity": "197"
+}
+update_date_full = datetime.datetime(year=2022, month=5, day=20)
+update_date_wo_hours_and_minutes = update_date_full.strftime("%Y%m%d")
+UPDATE_PIXEL_ENDPOINT = f"{POST_PIXEL_PIXELA_ENDPOINT}/{update_date_wo_hours_and_minutes}"
+
+# Update Pixel
+update_pixel_response = requests.put(url=UPDATE_PIXEL_ENDPOINT,
+                                     headers=pixela_headers,
+                                     json=update_pixel_parameters)
+
+print(update_pixel_response.text)
